390d32fba4f8316120e9c492572c3e7c29a0e61a
Claus Hoefele <claus@claushoefele.com>
2013-02-12 20:18:55 +0100

    Finished converting stolpersteine

--- import/kss_berlin.js
+++ import/kss_berlin.js
@@ -13,6 +13,7 @@ var source = {
 	name: "Koordinierungsstelle Stolpersteine Berlin"
 }
 
+// Request data
 console.log('Loading source data...');
 request({ uri:uriSource, headers: {'user-agent' : userAgent } }, function(error, response, body) {
 	console.log('Loading source data done');
@@ -21,6 +22,7 @@ request({ uri:uriSource, headers: {'user-agent' : userAgent } }, function(error,
 		return;
   }
 
+	// Parse XML
 	console.log('Parsing source data...');
 	parseXml(body, function (err, result) {
 		console.log('Parsing source data done');
@@ -29,12 +31,15 @@ request({ uri:uriSource, headers: {'user-agent' : userAgent } }, function(error,
 			return;
 	  }
 
+		// Process marker tags
 		console.log('Found ' + result.markers.$.cnt + ' stolpersteine in ' + result.markers.marker.length + ' markers');
 		var stolpersteine = [];
 		source.retrievedAt = 	new Date(response.headers["date"]);
 		var markers = result.markers.marker;
-		markers = markers.slice(0, 10);	// restrict test data
-		async.forEachLimit(markers, 1, function(marker, callback) {
+//		markers = markers.slice(250, 260); // restrict test data
+		for (var j = 0; j < markers.length; j++) {
+			var marker = markers[j];
+			
 			var location = {
 				street : marker.$.adresse,
 				city : "Berlin",
@@ -45,71 +50,45 @@ request({ uri:uriSource, headers: {'user-agent' : userAgent } }, function(error,
 					latitude: marker.$.lat
 				}
 			};
+
+			// Convert person tags
+			for (var i = 0; i < marker.person.length; i++) {
+				var person = marker.person[i];
+				console.log('- ' + person.$.name + ' (converting...)');
+				var stolperstein = convertStolperstein(person, location, source);
+				stolpersteine.push(stolperstein);
+				console.log('- ' + person.$.name + ' (converting done)');
+			}
 			
-			var stolperstein = {};
-			stolperstein.location = location;
-			stolperstein.source = source;
-			console.log(stolperstein);
-			callback();
-		});
+			// Convert person tags nested in 'weitere'
+			if (marker.weitere) {
+				console.log('Processing "weitere"');
+				for (var i = 0; i < marker.weitere.length; i++) {
+					location.street = marker.weitere[i].$.adresse;
+				
+					for (var k = 0; k < marker.weitere[i].person.length; k++) {
+						var person = marker.weitere[i].person[k];
+						console.log('- ' + person.$.name + ' (converting...)');
+						var stolperstein = convertStolperstein(person, location, source);
+						stolpersteine.push(stolperstein);
+						console.log('- ' + person.$.name + ' (converting done)');
+					}
+				}
+			}
+		};
+		console.log('Converted ' + stolpersteine.length + ' stolperstein(e)');
 	});
 });
 
-function convertStolperstein($, tableRow, callback) {
-	var stolperstein = {};
-	var itemRows = $(tableRow).find('td');
-			
-	// Person
-	var nameSpan = $(itemRows[1]).find('span');
-	if (nameSpan.find('span').length) {
-		nameSpan = nameSpan.find('span');
-	}
-	var names = nameSpan.text().split(',');
-	stolperstein.person = {
-		lastName: names[0].trim(),
-		firstName: names[1].trim()
+function convertStolperstein(person, location, source) {
+	return stolperstein = {
+		person : {
+			firstName : person.name,
+			lastName : ''
+		},
+		location : location,
+		source : source
 	};
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (converting...)');
-
-	// Image
-	var imageTag = $(itemRows[0]).find('img');
-	stolperstein.imageUrl = uriSource.protocol + imageTag.attr('src');
-	stolperstein.imageUrl = stolperstein.imageUrl.replace('/100px-', '/1024px-'); // width
-			
-	// Location
-	stolperstein.location = {
-		street: $(itemRows[2]).text().trim(),
-		city: "Berlin"
-	};
-			
-	// Laid at date
-	stolperstein.laidAt = {};
-	var laidAtSpan = $(itemRows[3]).find('span');
-	if ($(laidAtSpan).find('span').length) {
-		laidAtSpan = laidAtSpan.find('span');
-	}
-	var laidAtDates = laidAtSpan.text().split('-');
-	if (laidAtDates[0]) {
-		var number = new Number(laidAtDates[0]);
-		if (number != 0) {
-			stolperstein.laidAt.year = number;
-		}
-	}
-	if (laidAtDates[1]) {
-		var number = new Number(laidAtDates[1]);
-		if (number != 0) {
-			stolperstein.laidAt.month = number;
-		}
-	}
-	if (laidAtDates[2]) {
-		var number = new Number(laidAtDates[2]);
-		if (number != 0) {
-			stolperstein.laidAt.date = number;
-		}
-	}
-	
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (converting done)');
-	callback(null, stolperstein);
 }
 
 function patchStolperstein(stolperstein, callback) {
@@ -120,14 +99,5 @@ function patchStolperstein(stolperstein, callback) {
 	stolperstein.location.street = stolperstein.location.street.replace("Bundesratufer 1", "Bundesratufer 2");
 
 	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (patching done)');
-	callback(null, stolperstein);
-}
-
-function addSourceToStolperstein(source, stolperstein, callback) {
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (adding source...)');
-	
-	stolperstein.source = source;
-
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (adding source done)');
 	callback(null, stolperstein);
 }
