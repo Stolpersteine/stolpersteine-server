32c9f75ac159f9ab9a5c23b03409c3eab720c3a5
Claus Höfele <claus@claushoefele.com>
2013-04-01 16:19:56 +0200

    Removed gzip option as it's not needed any more

--- import/kss_berlin.js
+++ import/kss_berlin.js
@@ -7,7 +7,6 @@ var restify = require('restify'),
 	
 var apiClient = restify.createJsonClient({
 	version: '*',
-	gzip: true,
 	userAgent: 'Stolpersteine/1.0 (http://option-u.com; admin@option-u.com)',
 //	url: 'http://127.0.0.1:3000'
 	url: 'https://stolpersteine-optionu.rhcloud.com'
@@ -15,7 +14,6 @@ var apiClient = restify.createJsonClient({
 
 var kssUrl = 'http://www.stolpersteine-berlin.de/st_interface/xml/geo/linked';
 var kssClient = restify.createStringClient({
-	gzip: true,
 	userAgent: 'Stolpersteine/1.0 (http://option-u.com; admin@option-u.com)',
 	url: kssUrl
 });
@@ -97,6 +95,7 @@ kssClient.get('', function(error, req, res, data) {
 				console.log('Error during import (' + err + ')');
 			} else {
 				console.log(data);
+				console.log('Done.')
 			}
 		});
 	});
