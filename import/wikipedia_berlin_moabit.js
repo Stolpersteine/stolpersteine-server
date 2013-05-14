eea421b24e9df25cabff1cebcf0f14e6341910be
Claus Höfele <claus@claushoefele.com>
2013-05-14 22:38:31 +0200

    WIP parsing images from Wikipedia

--- import/wikipedia_berlin_moabit.js
+++ import/wikipedia_berlin_moabit.js
@@ -8,14 +8,15 @@
 var request = require('request'),
 	jsdom = require('jsdom'),
 	url = require('url'),
+	util = require('util'),
 	async = require('async');
 	
 var uriSource = url.parse('http://de.m.wikipedia.org/wiki/Liste_der_Stolpersteine_in_Berlin-Moabit');
 //var urlApi = 'http://127.0.0.1:3000/api';
 var urlApi = 'https://stolpersteine-optionu.rhcloud.com/api';
 var userAgent = 'Stolpersteine/1.0 (http://option-u.com; stolpersteine@option-u.com)';
-var additionalGeoHint = ',Moabit';
-var counter = 0;
+
+var numImages = 0;
 
 request({ uri:uriSource, headers: {'user-agent' : userAgent } }, function(error, response, body) {
   if (error) {
@@ -38,15 +39,15 @@ request({ uri:uriSource, headers: {'user-agent' : userAgent } }, function(error,
 		var $ = window.jQuery;
 		var tableRows = $('table.wikitable.sortable tr');
 		tableRows = tableRows.slice(1, tableRows.length); // first item is table header row
-		console.log('num table rows: ' + tableRows.length);
+		console.log('Num table rows: ' + tableRows.length);
 //		tableRows = tableRows.slice(91, 92);	// restrict test data
 		async.forEachLimit(tableRows, 1, function(tableRow, callback) {
 			async.waterfall([
 				convertStolperstein.bind(undefined, $, tableRow),
 				patchStolperstein,
-				addSourceToStolperstein.bind(undefined, source),
-				geocodeStolperstein
+				addSourceToStolperstein.bind(undefined, source)
 			], function(err, stolperstein) {
+				console.log(util.inspect(stolperstein));
 				if (!err) {
 					stolpersteine.push(stolperstein);
 				} else {
@@ -55,17 +56,16 @@ request({ uri:uriSource, headers: {'user-agent' : userAgent } }, function(error,
 				callback(err);
 			});
 		}, function() {
-			console.log('Done processing stolpersteine');
-			console.log('counter = ' + counter + '/' + stolpersteine.length);
+			console.log('Done processing ' + stolpersteine.length + ' stolperstein(e), ' + numImages + ' image(s)');
 			var importData = {
 				source: source,
 				stolpersteine: stolpersteine
 			};
-			console.log('importData = ' + importData);
-			request.post({url: urlApi + '/imports', json: importData}, function(err, res, data) {
-				console.log('Import (' + response.statusCode + ' ' + err + ')');
-				console.log(data);
-			});
+//			console.log('importData = ' + importData);
+//			request.post({url: urlApi + '/imports', json: importData}, function(err, res, data) {
+//				console.log('Import (' + response.statusCode + ' ' + err + ')');
+//				console.log(data);
+//			});
 		});
 	});
 });
@@ -84,7 +84,7 @@ function convertStolperstein($, tableRow, callback) {
 		lastName: names[0].trim(),
 		firstName: names[1].trim()
 	};
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (converting...)');
+	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName);
 
 	// Image
 	var imageTag = $(itemRows[0]).find('img');
@@ -124,101 +124,23 @@ function convertStolperstein($, tableRow, callback) {
 		}
 	}
 	
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (converting done)');
 	callback(null, stolperstein);
 }
 
 function patchStolperstein(stolperstein, callback) {
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (patching...)');
+	if (/Photo-request.svg.png$/.test(stolperstein.imageUrl)) {
+		delete stolperstein.imageUrl;
+	}
 	
-	stolperstein.location.street = stolperstein.location.street.replace("(heute Eingang U-Bahnhof Turmstraße)", "");
-	stolperstein.location.street = stolperstein.location.street.replace("Spenerstraße Ecke Melanchtonstraße", "Spenerstr. 14");
-	stolperstein.location.street = stolperstein.location.street.replace("Bundesratufer 1", "Bundesratufer 2");
-
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (patching done)');
+	if (stolperstein.imageUrl) {
+		numImages++;
+	}
+	
 	callback(null, stolperstein);
 }
 
 function addSourceToStolperstein(source, stolperstein, callback) {
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (adding source...)');
-	
 	stolperstein.source = source;
-
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (adding source done)');
-	callback(null, stolperstein);
-}
-
-function geocodeStolperstein(stolperstein, callback) {
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (geocoding...)');
 	
-	geocodeAddressMemoized(stolperstein.location.street, stolperstein.location.city, function(err, result) {
-		if (result) {
-			stolperstein.location.zipCode = result.zipCode;
-			stolperstein.location.coordinates = {
-				longitude: result.longitude,
-				latitude: result.latitude
-			};
-		}
-		console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName + ' (geocoding done, err: ' + err + ')');
-		callback(err, stolperstein);
-	});
-}
-
-var geocodeAddressMemoized = async.memoize(geocodeAddressRateLimited);
-function geocodeAddressRateLimited(street, city, callback)	{
-	setTimeout(geocodeAddress, 400, street, city, callback);
-}
-
-function geocodeAddress(street, city, callback) {
-	counter++;
-	var encodedStreet = encodeURIComponent(street);
-	var encodedCity = encodeURIComponent(city);
-	var uriGeocode = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&components=country:de&address=' + encodedStreet + ',' + encodedCity + additionalGeoHint;
-	request({ uri:uriGeocode, headers: {'user-agent' : userAgent } }, function(error, response, body) {
-		if (error) {
-			console.log('Error when contacting maps.googleapis.com site');
-			callback(new Error('Error result geocoding'));
-		}
-			
-		body = JSON.parse(body);
-		if (body.results.length === 0) {
-			console.log('Error result geocoding ' + body + '(' + uriGeocode + ')' );
-			callback(new Error('Error result geocoding'));
-		}
-		
-		var zipCode;
-		var addressComponents = body.results[0].address_components;
-		for (var i = 0; i < addressComponents.length; i++) {
-			var types = addressComponents[i].types;
-			for (var j = 0; j < types.length; j++) {
-				if (types[j] === "postal_code") {
-					zipCode = addressComponents[i].long_name;
-					break;
-				}
-			}
-			
-			if (zipCode) {
-				break;
-			}
-		}
-		
-		var longitude = body.results[0].geometry.location.lng;
-		var latitude = body.results[0].geometry.location.lat;
-		
-		if (zipCode === undefined || !longitude || !latitude) {
-			console.log('Missing valid geocode data ' + street + ', ' + city);
-			callback(new Error('Error result geocoding'));
-		}
-		
-		callback(null, {
-			zipCode: zipCode,
-			longitude: longitude,
-			latitude: latitude
-		});
-	});
-}
-
-function logStolperstein(stolperstein) {
-	console.log('- ' + stolperstein.person.lastName + ', ' + stolperstein.person.firstName);
-//	console.log(stolperstein);
+	callback(null, stolperstein);
 }
